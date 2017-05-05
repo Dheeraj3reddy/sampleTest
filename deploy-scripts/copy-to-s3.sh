a665834082f51571ec5b05b68d148f9cea85c5e6
Shannon Hickey <shickey@adobe.com>
2017-05-05 15:45:20 -0400

    Additional changes to support assuming role.

--- deploy-scripts/copy-to-s3.sh
+++ deploy-scripts/copy-to-s3.sh
@@ -8,25 +8,32 @@ if [ -z "$S3_BUCKETS" ]; then
     exit 1
 fi
 
-if [ -n "$AWS_ROLE" ]; then
-    echo Preparing CLI to assume role $AWS_ROLE
-    mkdir ~/.aws
+echo Preparing environment
 
-    cat > ~/.aws/credentials << EOF
+mkdir ~/.aws
+
+cat > ~/.aws/config << EOF
 [default]
 aws_access_key_id=$AWS_ACCESS_KEY_ID
 aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 aws_session_token=$AWS_SESSION_TOKEN
 EOF
 
-    cat > ~/.aws/config << EOF
-[profile deploy]
+if [ -n "$AWS_ROLE" ]; then
+    echo CLI will assume role $AWS_ROLE
+
+    # append the role
+    cat >> ~/.aws/config << EOF
 role_arn = $AWS_ROLE
-source_profile = default
+source_profile=default
 EOF
 
-    export AWS_DEFAULT_PROFILE=deploy
 fi
+
+# These need to be unset for the CLI to read config from ~/.aws/config
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
 
 TOP_LEVEL_MAX_AGE=60
 VERSIONED_MAX_AGE=86400
@@ -46,16 +53,16 @@ do
     echo ---------- Pushing to S3: $bucket ----------
 
     echo "--- Pushing dist$path_prefix/$sha/* into dist$path_prefix/$sha with max-age=$VERSIONED_MAX_AGE ---"
-    aws s3 cp --profile=deploy dist$path_prefix/$sha s3://$bucket/dist$path_prefix/$sha --recursive --cache-control max-age=$VERSIONED_MAX_AGE
+    aws s3 cp dist$path_prefix/$sha s3://$bucket/dist$path_prefix/$sha --recursive --cache-control max-age=$VERSIONED_MAX_AGE
 
     # Push top level assets AFTER pushing sha assets so we don't publish a reference to sha assets before they're available
     echo "--- Pushing dist$path_prefix/* (excluding dist$path_prefix/$sha and dist$path_prefix/sha.txt) into dist$path_prefix with max-age=$TOP_LEVEL_MAX_AGE ---"
-    aws s3 cp --profile=deploy dist$path_prefix s3://$bucket/dist$path_prefix --recursive --exclude $sha/* --exclude sha.txt --cache-control max-age=$TOP_LEVEL_MAX_AGE
+    aws s3 cp dist$path_prefix s3://$bucket/dist$path_prefix --recursive --exclude $sha/* --exclude sha.txt --cache-control max-age=$TOP_LEVEL_MAX_AGE
 
     echo "--- Pushing dist$path_prefix/sha.txt to dist$path_prefix/sha.txt with max-age=$TOP_LEVEL_MAX_AGE ---"
-    aws s3 cp --profile=deploy dist$path_prefix/sha.txt s3://$bucket/dist$path_prefix/sha.txt --cache-control max-age=$TOP_LEVEL_MAX_AGE
+    aws s3 cp dist$path_prefix/sha.txt s3://$bucket/dist$path_prefix/sha.txt --cache-control max-age=$TOP_LEVEL_MAX_AGE
 
     echo "--- Pushing build-artifacts/manifests$path_prefix/$sha.txt to manifests$path_prefix/$sha.txt ---"
-    aws s3 cp --profile=deploy build-artifacts/manifests$path_prefix/$sha.txt s3://$bucket/manifests$path_prefix/$sha.txt
+    aws s3 cp build-artifacts/manifests$path_prefix/$sha.txt s3://$bucket/manifests$path_prefix/$sha.txt
 done
 
