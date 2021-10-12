8fd49a81aa7b083442174abe5486c794d0d9370d
Andrew Lau <an.lau@adobe.com>
2021-10-12 09:25:59 -0700

    Switch tests to jest execution

--- js/nls/ui-strings.js
+++ js/nls/ui-strings.js
@@ -2,28 +2,31 @@
  * Module that handles lazy loading translated strings as json object and provide helper method to return translated string
  * for a given key.
  *
- * Peforms two functions:
- *  - at compiel/bundle time, uses 'bundle-loader' to generate lazy-loadable chunks for each locale.
+ * Performs two functions:
+ *  - at compile/bundle time, uses 'bundle-loader' to generate lazy-loadable chunks for each locale.
  *  - at run time, provides loadTranslations() method that lazy load locale and helper method to get translated string by key
  */
 
 /* global require, module */
 
 var Promise = require('es6-promise').Promise;
+// require('bundle-loader!./root/ui-strings.json');
+
 
 // Module level variable that stores translations (as key/value object)
 var loadedTranslations = null;
 
 /**
- * Loads tranlsations based on the locale and stores it as loadedTranslations variable, that later can be used
+ * Loads translations based on the locale and stores it as loadedTranslations variable, that later can be used
  * via getTranslatedString() method.
  * @param {String} locale to be loaded
- * @returns {Promise} the promise that is resolved when translated file is loaded and getTranslatdString can be used.
+ * @returns {Promise} the promise that is resolved when translated file is loaded and getTranslatedString can be used.
  */
 function loadTranslations(locale) {
   var loc = (locale === 'en_US') ? 'root' : locale;
   return new Promise(function (resolve) {
-    require('bundle-loader?lazy&name=[folder]!./' + loc + '/ui-strings.json')(function (jsonBundle) {
+    // require('bundle-loader?lazy&name=[folder]!./' + loc + '/ui-strings.json')(function (jsonBundle) {
+    require('bundle-loader!./'+ loc +'/ui-strings.json')(function (jsonBundle) {
       loadedTranslations = jsonBundle;
       resolve(jsonBundle);
     });
--- test/lib-test.js
+++ test/lib-test.js
@@ -4,37 +4,36 @@
 /* eslint-env mocha */
 /* global require, describe */
 
-var expect = require('chai').expect,
-  UiStrings = require('../js/nls/ui-strings'),
-  lib = require('../js/lib'),
-  jQuery = require('jquery'),
-  _ = require('underscore');
+var UiStrings = require('../js/nls/ui-strings'),
+    lib = require('../js/lib'),
+    jQuery = require('jquery'),
+    _ = require('underscore');
 
-describe('App Library Functions', function() {
-  before(function () {
+describe('App Library Functions', () => {
+  beforeAll(() => {
     return UiStrings.loadTranslations('en_US');
   });
 
-  it('sets hello-world message', function() {
+  it('sets hello-world message', () => {
     lib.sayHello();
-    expect(jQuery('.title').html()).to.equal(UiStrings.getTranslatedString('helloWorldMsg'));
+    expect(jQuery('.title').html()).toBe(UiStrings.getTranslatedString('helloWorldMsg'));
   });
 
-  it('shows image info', function () {
+  it('shows image info', () => {
     var numImages, listItems, imageElements;
     lib.showImageInfo();
 
     // number of images should be 2
     numImages = jQuery('.image-info > div > span').html();
-    expect(numImages).to.equal('2');
+    expect(numImages).toBe('2');
 
     // we should have 2 list items whose values matching the images' src attributes.
     listItems = jQuery('.image-info > ul li');
     imageElements = jQuery('.sample-image img');
-    expect(listItems.length).to.equal(imageElements.length);
+    expect(listItems.length).toBe(imageElements.length);
     _.each(listItems, function (item, index) {
       var imageLink = jQuery(imageElements[index]).attr('src');
-      expect(item.innerHTML).to.equal(imageLink);
+      expect(item.innerHTML).toBe(imageLink);
     });
   })
 });
