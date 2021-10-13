8d9ea5f616c18801f718876762667c00f05392cb
Andrew Lau <an.lau@adobe.com>
2021-10-13 10:22:49 -0700

    Integrate with SonarQube

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ fi
 rm -rf dist dist-pub
 npm install
 npm run build
-npm run test
+npm run coverage
 
 # SonarQube analysis
 if [ -n "$SONAR_TOKEN" ]; then
@@ -48,7 +48,7 @@ if [ -n "$SONAR_TOKEN" ]; then
         exit 1
     fi
 
-    ############# TODO - Add SonarQube analysis here #############
+    npm run test:sonar
 fi
 
 # Publish an NPM package if $PUSH_ARTIFACTS is non-empty and the "dist-pub"
